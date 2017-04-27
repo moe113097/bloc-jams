56f950ab7414ad5d8cf5f48ffaa667594c23f8c5
moe113097 <moe113097@gmail.com>
2017-04-27 14:49:15 -0400

    modified landing

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,18 +1,17 @@
 
 var pointsArray= document.getElementsByClassName('point');
-var animatePoints = function(points){
-
-  var revealPoint=function(index){
-    //for (var i=0;i<points.length;i++){
-      points[index].style.opacity=1;
-      points[index].style.transform="scaleX(1) translateY(0)";
-      points[index].style.msTransform="scaleX(1) translateY(0)";
-      points[index].style.WebkitTransform="scaleX(1) translateY(o)";
-  }
-  for (var i=0;i<pointsArray.length;i++){
-    revealPoint(i);
-  };
+var revealPoint=function(point){
+    point.style.opacity=1;
+    point.style.transform="scaleX(1) translateY(0)";
+    point.style.msTransform="scaleX(1) translateY(0)";
+    point.style.WebkitTransform="scaleX(1) translateY(o)";
 }
+
+var animatePoints = function(points){
+    forEach(points,revealPoint);
+
+  };
+
   window.onload=function(){
     // automatically animate the points on a tall screen where scrolling  can't trigger the animation
     if (window.innerHeight>950){
@@ -26,28 +25,3 @@ var animatePoints = function(points){
           }
     });
   }
-
-/*  var revealFirstPoint=function(){
-      points[0].style.opacity=1;
-      points[0].style.transform="scaleX(1) translateY(0)";
-      points[0].style.msTransform="scaleX(1) translateY(0)";
-      points[0].style.WebkitTransform="scaleX(1) translateY(0)";
-  };
-  var revealSecondPoint =function(){
-      points[1].style.opacity=1;
-      points[1].style.transform="scaleX(1) translateY(0)";
-      points[1].style.msTransform="scaleX(1) translateY(0)";
-      points[1].style.WebkitTransform= "scaleX(1) translateY(0)";
-
-  };
-  var revealThirdPoint = function(){
-    points[2].style.opacity =1;
-    points[2].style.transform="scaleX(1) translateY(0)";
-    points[2].style.msTransform ="scaleX(1) translateY(0)";
-    points[2].style.WebkitTransform= "scaleX(1) translateY(0)";
-  };
-
-  revealFirstPoint();
-  revealSecondPoint();
-  revealThirdPoint();
-};*/
