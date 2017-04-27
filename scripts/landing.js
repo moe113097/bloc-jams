fec837ae5e5264dde9f3c6d45b7378a7beef4e3a
moe113097 <moe113097@gmail.com>
2017-04-27 12:23:07 -0400

    added window onload

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function(){
 
-  var points=document.getElementsByClassName('point');
+var pointsArray= document.getElementsByClassName('point');
+var animatePoints = function(points){
 
   var revealPoint=function(){
     for (var i=0;i<points.length;i++){
@@ -12,6 +12,19 @@ var animatePoints = function(){
   }
     revealPoint();
   };
+  window.onload=function(){
+    // automatically animate the points on a tall screen where scrolling  can't trigger the animation
+    if (window.innerHeight>950){
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top -window.innerHeight +200;
+    window.addEventListener('scroll',function(event){
+          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+                  animatePoints(pointsArray);
+          }
+    });
+  }
 
 /*  var revealFirstPoint=function(){
       points[0].style.opacity=1;
