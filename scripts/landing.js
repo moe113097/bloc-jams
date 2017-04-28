ca593d75a1b4e0985a55c3b311edb7a539af150a
moe113097 <moe113097@gmail.com>
2017-04-27 20:11:51 -0400

    collection script added

--- scripts/landing.js
+++ scripts/landing.js
@@ -4,7 +4,7 @@ var revealPoint=function(point){
     point.style.opacity=1;
     point.style.transform="scaleX(1) translateY(0)";
     point.style.msTransform="scaleX(1) translateY(0)";
-    point.style.WebkitTransform="scaleX(1) translateY(o)";
+    point.style.WebkitTransform="scaleX(1) translateY(0)";
 }
 
 var animatePoints = function(points){
